7cb8962faaeb0629945593fb8da9aace7aa90ab4
irvinnlim <irvinnlim@gmail.com>
2023-04-29 22:30:17 +0800

    fix admin-security bug

--- admin/pages/api/auth/[...nextauth].js
+++ admin/pages/api/auth/[...nextauth].js
@@ -28,7 +28,6 @@ export default NextAuth(authOptions);
 
 export async function isAdminRequest(req, res) {
   const session = await getServerSession(req, res, authOptions);
-  res.status();
   if (!adminEmails.includes(session?.user?.email)) {
     res.status(401);
     res.end();
